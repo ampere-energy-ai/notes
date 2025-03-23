67845cf94b0cdb62b7bcbac7a902e35292c5a311
Chris Jones <chris@amperefinancial.com>
2025-03-23 14:00:47 -0400

    Add API

---
+++ packages/functions/src/update.ts
@@ -0,0 +1,26 @@
+import { Resource } from "sst";
+import { Util } from "@notes/core/util";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { UpdateCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const dy = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const main = Util.handler(async (event) => {
+	const data = JSON.parse(event.body || "{}");
+	const params = {
+		TableName: Resource.Notes.name,
+		Key: {
+			userId: "123",
+			noteId: event?.pathParameters?.id,
+		},
+		UpdateExpression: "SET content = :content, attachment = :attachment",
+		ExpressionAttributeValues: {
+			":attachment": data.attachment || null,
+			":content": data.content || null,
+		},
+	};
+
+	await dy.send(new UpdateCommand(params));
+	return JSON.stringify({status: true});
+});
+
